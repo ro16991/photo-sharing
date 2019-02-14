ec231a2ce914b27ed692a3a46bd9ce31dac8a1ee
ro16991 <44852173+ro16991@users.noreply.github.com>
2019-02-14 08:38:09 +0000

    Update photo.js

--- photo.js
+++ photo.js
@@ -2,12 +2,14 @@ var albumBucketName = 'www.peeka-boo.com';
 var bucketRegion = 'eu-west-2';
 var IdentityPoolId = 'us-east-2:bb28458e-d1f1-45b7-9a10-986617014c29';
 
-AWS.config.update({
-  region: bucketRegion,
-  credentials: new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: IdentityPoolId
-  })
-});
+
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'REGION'});
+
+
+// Create S3 service object
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 var s3 = new AWS.S3({
   apiVersion: '2006-03-01',
