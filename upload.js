21ba33105dd2f7e69285b938d72b09be06c396b8
ro16991 <44852173+ro16991@users.noreply.github.com>
2019-02-14 08:40:17 +0000

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,6 +1,6 @@
 var AWS = require('aws-sdk');
 // Set the region 
-AWS.config.update({region: 'REGION'});
+AWS.config.update({region: 'eu-west-2'});
 
 // Create S3 service object
 s3 = new AWS.S3({apiVersion: '2006-03-01'});
