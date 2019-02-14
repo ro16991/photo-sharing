8513be84dfaf4e9dbb60117e91bbbf9df2da0164
ro16991 <44852173+ro16991@users.noreply.github.com>
2019-02-14 09:19:27 +0000

    Update photo.js

--- photo.js
+++ photo.js
@@ -5,8 +5,14 @@ var IdentityPoolId = 'us-east-2:bb28458e-d1f1-45b7-9a10-986617014c29';
 
 var AWS = require('aws-sdk');
 // Set the region 
-AWS.config.update({region: 'REGION'});
+AWS.config.update({region: bucketregion});
 
+AWS.config.update({
+  region: bucketRegion,
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: IdentityPoolId
+  })
+});
 
 // Create S3 service object
 s3 = new AWS.S3({apiVersion: '2006-03-01'});
